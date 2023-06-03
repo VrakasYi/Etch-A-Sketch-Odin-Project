fb2686d8bcdb9367354bd3a2aebe1292036e0265
VrakasYi <vrakasyi@outlook.com>
2023-06-03 11:27:02 +0300

    can now create div rows

--- etch-js.js
+++ etch-js.js
@@ -1,6 +1,9 @@
 const container = document.querySelector('#container');
 const div = document.createElement('div');
+
 for (let i = 0; i <= 15; i++) {
+    const div = document.createElement('div');
     container.appendChild(div);
-    div.classList.add('pixel')
-}
+    div.classList.add('row');
+    div.textContent = 'row';
+};
