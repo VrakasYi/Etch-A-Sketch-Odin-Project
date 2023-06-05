c5d0f674b9d095219bfe56e3c032b41c9bb6d6c4
VrakasYi <vrakasyi@outlook.com>
2023-06-05 18:24:29 +0300

    achieved grid using rowxcolumn divs inside a flex-direction column container

--- etch-js.js
+++ etch-js.js
@@ -1,11 +1,17 @@
 const container = document.querySelector('#container');
-const rowDiv = document.createElement('div');
-rowDiv.classList.add('row');
-container.appendChild(rowDiv)
+let gridDivsNo = document.getElementById('container').className-1;
 
-for (let i = 0; i <= 15; i++) {
-    const colDiv = document.createElement('div');
-    colDiv.classList.add('column');
-    colDiv.textContent = 'pxl';
-    rowDiv.appendChild(colDiv);
-};
+//Create a div with class row with container parent
+for (let i = 0; i <= gridDivsNo; i++) {
+    let rowDiv = document.createElement('div');
+    rowDiv.classList.add('row');
+    container.appendChild(rowDiv);
+
+    //Create a column class of divs with row divs as parent
+    for (let j = 0; j <= gridDivsNo; j++) {
+        let colDiv = document.createElement('div');
+        colDiv.classList.add('column');
+        colDiv.textContent = i;
+        rowDiv.appendChild(colDiv);
+    };
+}
